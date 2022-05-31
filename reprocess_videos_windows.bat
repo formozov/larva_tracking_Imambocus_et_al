4e3dbbaddf1af8cdab0d755b7e9acf72a8fdecd7
formozov <formozoff@gmail.com>
2022-05-31 14:07:34 +0200

    Final version

--- reprocess_videos_windows.bat
+++ reprocess_videos_windows.bat
@@ -1,12 +1,14 @@
 :: Locate this script in the folder with *.mp4 files
 :: Run Command prompt in Windows
 :: Using Command prompt go to the folder with *.mp4 files by typing:
-:: cd /Users/..../path_to_files
+:: cd C:\Users\....\path_to_files\
 :: In case the files are located not on C: hard drive but on the other disk (for example Q:), type
 :: Q:
 :: to switch to this hard drive
+:: Run script by typing
+:: reprocess_videos_windows.bat
 
-ECHO "TEST VERSION: DO NOT USE"
+ECHO "Analysis script"
 
 mkdir reduce
 mkdir crop
