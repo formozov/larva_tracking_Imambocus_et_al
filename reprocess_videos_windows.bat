411c146a41f327bd2bdba00b98eddfc8de3c2256
formozov <formozoff@gmail.com>
2022-05-31 13:58:58 +0200

    after modification of the Path variable

--- reprocess_videos_windows.bat
+++ reprocess_videos_windows.bat
@@ -5,9 +5,9 @@ mkdir reduce
 mkdir crop
 mkdir avi
 
-for %%f in (*.mpg) do (
+for %%f in (*.mp4) do (
 echo "%%f"
-Q:\Andrey\Drosophila\LARVATRACK\ProtocolTest\ffmpeg\ffmpeg-2021-12-20-git-631e31773b-essentials_build\ffmpeg-2021-12-20-git-631e31773b-essentials_build\bin\ffmpeg.exe -i "%%f" -vf "select=not(mod(n\,200))" -vsync vfr -q:v 2 reduce/"%%f"
-Q:\Andrey\Drosophila\LARVATRACK\ProtocolTest\ffmpeg\ffmpeg-2021-12-20-git-631e31773b-essentials_build\ffmpeg-2021-12-20-git-631e31773b-essentials_build\bin\ffmpeg.exe -i reduce/"%%f" -filter:v "crop=1288:600:0:200" -c:a copy crop/"%%f"
-Q:\Andrey\Drosophila\LARVATRACK\ProtocolTest\ffmpeg\ffmpeg-2021-12-20-git-631e31773b-essentials_build\ffmpeg-2021-12-20-git-631e31773b-essentials_build\bin\ffmpeg.exe -i crop/"%%f" -pix_fmt nv12 -f avi -vcodec rawvideo avi/"%%f"
+ffmpeg -i "%%f" -vf "select=not(mod(n\,200))" -vsync vfr -q:v 2 reduce/"%%f"
+ffmpeg -i reduce/"%%f" -filter:v "crop=1288:600:0:200" -c:a copy crop/"%%f"
+ffmpeg -i crop/"%%f" -pix_fmt nv12 -f avi -vcodec rawvideo avi/"%%f"
 )
