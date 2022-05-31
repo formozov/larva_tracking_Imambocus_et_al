475451d3a5aeed0f6d24b0aefb7a8cbe5304f6ea
formozov <formozoff@gmail.com>
2022-05-31 15:56:49 +0200

    .avi extension modified

--- reprocess_videos_windows.bat
+++ reprocess_videos_windows.bat
@@ -30,5 +30,5 @@ for %%f in (*.mp4) do (
 echo "%%f"
 ffmpeg -i "%%f" -vf "select=not(mod(n\,%take_each_n_frame%))" -vsync vfr -q:v 2 reduce/"%%f"
 ffmpeg -i reduce/"%%f" -filter:v "crop=%width%:%height%:%X%:%Y%" -c:a copy crop/"%%f"
-ffmpeg -i crop/"%%f" -pix_fmt nv12 -f avi -vcodec rawvideo avi/"%%f"
+ffmpeg -i crop/"%%f" -pix_fmt nv12 -f avi -vcodec rawvideo avi/"%%f.avi"
 )
